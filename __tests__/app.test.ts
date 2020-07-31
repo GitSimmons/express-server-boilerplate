8b38f7852be0799af2097eec875da19e03ea8390
Ben Simmons <ben.ryan.simmons@gmail.com>
2020-07-31 16:44:39 -0400

    completed coding assessment

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,53 +1,192 @@
 const request = require("supertest");
 import { app } from "../src/app";
 
+const arrayToSave = [
+  { author: "Steve McConnell", name: "Code Complete", published: 1993 },
+  {
+    author: "Steve McConnell",
+    name: "Rapid development",
+    published: 1996,
+  },
+  {
+    author: "Steve McConnell",
+    name: "After the gold rush",
+    published: 1999,
+  },
+  { author: "Martin Fowler", name: "Refactoring", published: 1999 },
+  { author: "Martin Fowler", name: "UML distilled", published: 1997 },
+];
+
 describe("API tests", () => {
-  it("should send 200 OK to GET root", async () => {
-    const response = await request(app).get("/");
-    expect(response.status).toBe(200);
-  });
-  describe("/addition", () => {
-    it("should add two numbers", async () => {
-      const response = await request(app)
-        .get("/addition")
-        .query({ a: "2", b: "3" });
-      expect(response.status).toBe(200);
-      expect(response.text).toBe("5");
-    });
-    it("should send a 400 when adding things that are not numbers", async () => {
-      const response = await request(app).get("/addition").query({
-        a: "abc",
-        b: "def",
-      });
-      expect(response.status).toBe(400);
-      expect(response.text).toMatch(/Bad Request/);
+  describe("/save", () => {
+    it("should save a JSON array to memory", async () => {
+      const postResponse = await request(app).post("/save").send(arrayToSave);
+      expect(postResponse.status).toBe(201);
+      expect(postResponse.text).toBe(JSON.stringify(arrayToSave));
     });
   });
-  describe("/memory", () => {
-    it("should add values to memory and be able to recall them", async () => {
-      const putResponse = await request(app).put("/memory").send("a=4&b=5");
-      expect(putResponse.status).toBe(201);
-      expect(putResponse.body).toMatchObject({
-        a: "4",
-        b: "5",
-      });
-      const response = await request(app).get("/memory");
+  describe("/load", () => {
+    it("should load the data saved to memory", async () => {
+      const postResponse = await request(app).post("/save").send(arrayToSave);
+      expect(postResponse.status).toBe(201);
+      const response = await request(app).get("/load");
       expect(response.status).toBe(200);
-      expect(response.body).toMatchObject({
-        a: "4",
-        b: "5",
+      expect(JSON.parse(response.text)).toEqual(arrayToSave);
+    });
+  });
+  describe("/search", () => {
+    beforeAll(async () => {
+      // add some sample data to memory
+      const postResponse = await request(app).post("/save").send(arrayToSave);
+      expect(postResponse.status).toBe(201);
+    });
+
+    it("should be able to search from exact text", async () => {
+      const searchResponse = await request(app)
+        .get("/search")
+        .query({ exact_text: "Steve%20McConnell" });
+      expect(searchResponse.status).toBe(200);
+      expect(JSON.parse(searchResponse.text)).toEqual(
+        arrayToSave.filter((book) => book.author === "Steve McConnell")
+      );
+    });
+    it("should be able to search for partial matches", async () => {
+      const searchResponse = await request(app)
+        .get("/search")
+        .query({ text: "Co" });
+      expect(searchResponse.status).toBe(200);
+      expect(JSON.parse(searchResponse.text)).toEqual(
+        arrayToSave.filter((book) => book.author === "Steve McConnell")
+      );
+    });
+    it("should be able to order by string fields", async () => {
+      const searchResponse = await request(app)
+        .get("/search")
+        .query({ text: "Steve", order_by: "name" });
+      expect(searchResponse.status).toBe(200);
+      expect(JSON.parse(searchResponse.text)).toEqual([
+        arrayToSave[2],
+        arrayToSave[0],
+        arrayToSave[1],
+      ]);
+    });
+    it("should be able to order by number fields", async () => {
+      const searchResponse = await request(app)
+        .get("/search")
+        .query({ text: "Steve", order_by: "published" });
+      expect(searchResponse.status).toBe(200);
+      expect(JSON.parse(searchResponse.text)).toEqual([
+        arrayToSave[0],
+        arrayToSave[1],
+        arrayToSave[2],
+      ]);
+    });
+    it("should not mutate memory when performing a search or sorting results", async () => {
+      const searchResponse = await request(app).get("/search").query({
+        text: "Steve",
+        order_by: "published",
+        sort_direction: "desc",
+      });
+      expect(searchResponse.status).toBe(200);
+      const memory = await request(app).get("/load");
+      expect(JSON.parse(memory.text)).toEqual(arrayToSave);
+    });
+    describe("/search?sort_direction='desc' should sort in descending order", () => {
+      const cases = [
+        // strings
+        [
+          {
+            text: "Steve",
+            order_by: "name",
+            sort_direction: "desc",
+          },
+          [arrayToSave[1], arrayToSave[0], arrayToSave[2]],
+        ],
+        // numbers
+        [
+          {
+            text: "Martin",
+            order_by: "published",
+            sort_direction: "desc",
+          },
+          [arrayToSave[3], arrayToSave[4]],
+        ],
+      ];
+      test.each(cases)(
+        "given %p, returns the correct order",
+        async (query, expectedResult) => {
+          const searchResponse = await request(app).get("/search").query(query);
+          expect(searchResponse.status).toBe(200);
+          expect(JSON.parse(searchResponse.text)).toEqual(expectedResult);
+        }
+      );
+    });
+  });
+  describe("Test it with data of a different kind", () => {
+    const someVeryDifferentData = [
+      {
+        method: "filter",
+        ratio: "1:8",
+        brew_time_in_seconds: 180,
+      },
+      {
+        method: "espresso",
+        ratio: "1:2",
+        brew_time_in_seconds: 25,
+      },
+      {
+        method: "inverted aeropress",
+        ratio: "1:12",
+        brew_time_in_seconds: 120,
+      },
+    ];
+    describe("/save", () => {
+      it("should save even with different data", async () => {
+        const response = await request(app)
+          .post("/save")
+          .send(someVeryDifferentData);
+        expect(response.status).toBe(201);
       });
     });
-    it("should get a single key from memory", async () => {
-      const putResponse = await request(app).put("/memory").send("a=4&b=5");
-      expect(putResponse.status).toBe(201);
-      const response = await request(app).get("/memory/a");
-      expect(response.status).toBe(200);
-      expect(response.text).toBe("4");
+    describe("/load", () => {
+      it("should load saved data", async () => {
+        const saveResponse = await request(app)
+          .post("/save")
+          .send(someVeryDifferentData);
+        expect(saveResponse.status).toBe(201);
+        const loadResponse = await request(app).get("/load");
+        expect(JSON.parse(loadResponse.text)).toEqual(someVeryDifferentData);
+      });
     });
-    it("should return 404 when getting a nonexistent memory", async () => {
-      const response = await request(app).get("/memory/test");
-      expect(response.status).toBe(404);
+    describe("/search", () => {
+      beforeAll(async () => {
+        const response = await request(app)
+          .post("/save")
+          .send(someVeryDifferentData);
+        expect(response.status).toBe(201);
+      });
+      it("search for exact text", async () => {
+        const searchResponse = await request(app)
+          .get("/search")
+          .query({ exact_text: "espresso" });
+        expect(searchResponse.status).toBe(200);
+        expect(JSON.parse(searchResponse.text)).toEqual([
+          someVeryDifferentData[1],
+        ]);
+      });
+      it("sort in descending order", async () => {
+        const searchResponse = await request(app).get("/search").query({
+          text: "e", // genius, right?
+          order_by: "brew_time_in_seconds",
+          sort_direction: "desc",
+        });
+        expect(searchResponse.status).toBe(200);
+        expect(JSON.parse(searchResponse.text)).toEqual([
+          someVeryDifferentData[0],
+          someVeryDifferentData[2],
+          someVeryDifferentData[1],
+        ]);
+      });
     });
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,95 @@
 import express from "express";
-//const express = require("express");
-// use a simple javascript oject to hold data in memory
-const memory: any = {};
+
+type BOOK = {
+  author: string;
+  name: string;
+  published: number;
+};
+
+enum DIRECTION {
+  ASCENDING = "asc",
+  DESCENDING = "desc",
+}
+
+let memory: BOOK[] = [];
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.set("query parser", (queryString: string) => {
   return new URLSearchParams(queryString);
 });
 
-app.get("/", (_: any, res: any) => {
-  res.status(200).send("GET /");
+app.post("/save", (req, res) => {
+  memory = req.body;
+  res.status(201).json(memory);
 });
-app.get("/addition", function ({ query: { a, b } }: any, res: any) {
-  const result = parseInt(a) + parseInt(b);
-  isNaN(result)
-    ? res
-        .status(400)
-        .send(
-          "Bad Request - Are you sure you specified two numbers? Did you forget to put the curl URL in quotes?"
-        )
-    : res.status(200).send(`${result}`);
+app.get("/load", (_, res) => {
+  res.status(200).json(memory);
 });
-app.get("/memory", (_, res) => {
-  res.status(200).send(memory);
-});
-app.put("/memory", (req, res) => {
-  for (const key in req.body) {
-    memory[key] = req.body[key];
+app.get("/search", function (
+  {
+    query: { exact_text, text, order_by, sort_direction },
+  }: {
+    query: {
+      exact_text: string;
+      text: string;
+      order_by: keyof BOOK;
+      sort_direction: DIRECTION;
+    };
+  },
+  res
+) {
+  type FIELD = keyof BOOK;
+  let results: BOOK[] = [];
+  // VALIDATORS:
+  // ensure that something is being searched for
+  if (!exact_text && !text) {
+    res.status(400).json({
+      error: "Please include a query parameter for either exact_text or text",
+    });
   }
-  res.status(201).send(memory);
-});
-
-app.get("/memory/:key", (req, res) => {
-  const { key } = req.params;
-  if (memory[key] !== undefined) {
-    res.status(200).send(memory[key]);
-  } else {
-    res.status(404).send("Key not found");
+  // ensure that not both exact and partial search are being used
+  if (exact_text && text) {
+    res.status(400).json({
+      error: "Can't search for both an exact match and partial match",
+    });
   }
+  // ensure that order_by is actually a field on the objects
+  const fieldsOnObject = Object.keys(memory[0]);
+  if (order_by && !fieldsOnObject.includes(order_by)) {
+    res.status(400).json({ error: "Invalid order_by field" });
+  }
+  const sortByText = (memory: BOOK[], order_by: FIELD) =>
+    memory.sort((a, b) => (a[order_by] > b[order_by] ? 1 : -1));
+  const sortByNumber = (memory: BOOK[], order_by: FIELD) =>
+    memory.sort((a, b) => a[order_by] - b[order_by]);
+  const sortSearch = (
+    results: BOOK[],
+    order_by: FIELD,
+    sort_direction: DIRECTION
+  ) => {
+    const orderedResults =
+      typeof results[0][order_by] === "number"
+        ? sortByNumber(results, order_by)
+        : typeof results[0][order_by] === "string"
+        ? sortByText(results, order_by)
+        : results;
+    return sort_direction === DIRECTION.DESCENDING
+      ? orderedResults.reverse()
+      : orderedResults;
+  };
+  // populate results based on exact text or partial text
+  results = memory.filter((book: BOOK) =>
+    Object.values(book).some(
+      (field) =>
+        field === decodeURI(exact_text) ||
+        field.toString().includes(decodeURI(text))
+    )
+  );
+  results
+    ? res.status(200).json(sortSearch(results, order_by, sort_direction))
+    : res.status(204).json({ error: "No Content Found" });
 });
 
 export { app };
